4d0e774926ca61f31a2af673260a6ad96e7b2289
ziadx3 <x6rbanx2@gmail.com>
2024-06-07 19:54:13 +0300

    define the movies route & use responser package & connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import {moviesRouter} from './api/routes/moviesrouter'
+import {connectToDatabase} from './api/services/database.s'
+import responser from 'responser'
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(responser)
+
+connectToDatabase()
+
+app.use('/movies',moviesRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript Node Express!');
