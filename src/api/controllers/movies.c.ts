7f178d668d73727c24093e9d66b70c16badaa232
ziadx3 <x6rbanx2@gmail.com>
2024-06-09 04:40:57 +0300

    finish movies controller buisness logic

--- src/api/controllers/movies.c.ts
+++ src/api/controllers/movies.c.ts
@@ -1,18 +1,21 @@
 import { Request, RequestHandler, Response } from "express";
 import { Movie } from "../models/movie";
-import {listMovies} from "../services/movies.s";
+import {getTimeslot, listMovies, reserveTimeslot} from "../services/movies.s";
+import { CheckMoviesRequest, ReserveTimeslotRequest } from "../models/movie.requests";
+import { Timeslot } from "../models/timeslot";
 
 
 
-
-
-
-
+/**
+ *  Movies controller handles the buisness logic for the movies route
+ */
 export class MoviesController {
 
-
-
-    // list all the available movies 
+    /**
+     *  list all the available movies 
+     * @param {Request} request
+     * @param {Response} response
+     */
     async listMovies(req : Request , res : Response) : Promise<void>{
         
         try {
@@ -20,7 +23,7 @@ export class MoviesController {
         const movies = await listMovies() 
 
         if(movies.length == 0)
-            return res.send_ok("There is no movies to be shown")
+            return res.send_ok("There is no movies available")
 
         return res.send_ok("Success",movies)
 
@@ -32,18 +35,58 @@ export class MoviesController {
         }
         
     }
+
+    /**
+     * checks if the movie is available in specific timeslot {returns available capacity}
+     * @param {Request} request
+     * @param {Response} response
+     */
+    async checkTimeslotCapacity(req: Request, res: Response) : Promise<void> {
+
+        try {
+            // fill the body object into {{CheckMoviesRequest object}}
+            const body = req.body as CheckMoviesRequest
+
+            // get the timeslot from the movie by the service
+            const timeslot = await getTimeslot(body.movieId!,body.timeslotId!) as Timeslot
     
-    // reserve a movie in a specific timeslot 
-    async reserveMovie() : Promise<void> {
+            // check if the timeslot isn't undefined then return the remaining cpacitiy
+            if(timeslot)
+                return res.send_ok("success",{
+                    "remaining capactiy": timeslot.remainingCapacity
+                })
+            
+            // otherwise it will return 404 not found
+            return res.send_notFound("Timeslot not found")
+        }
+        catch(error : any) {
 
-        // to be implemented
+            // in case un expected error it will return 500 error
+            return res.send_internalServerError(error.message)
+        }
+      
+
     }
-
-
-    // checks if the movie is available in specific timeslot
-    async checkAvailableMovie() : Promise<void> {
+    
+    /**
+     * reserve a movie in a specific timeslot 
+     * @param {Request} request
+     * @param {Response} response
+     */
+    async reserveTimeslot(req : Request,res : Response) : Promise<void> {
         
-        // to be implemented
+        // fill the body object into {{ReserveTimesslotRequest object}}  
+        const requestBody = req.body as ReserveTimeslotRequest
+
+        // reserve a timeslot by the service and return the response
+        const response = await reserveTimeslot(requestBody.movieId!,requestBody.timeslotId!,requestBody.uid!,requestBody.numberOfreserves!)
+
+        // checks if the response is successed (if so it will return ok response)
+        if(response.successed)
+            return res.send_ok(response.message)
+
+        // otherwise it will return an internal error
+        return res.send_internalServerError(response.message)
 
     }
 
