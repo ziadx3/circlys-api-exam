0ba3d8c88808d748f66604c90f4b3235d8436c15
ziadx3 <x6rbanx2@gmail.com>
2024-06-09 04:46:22 +0300

    add movies route middleware

---
+++ src/api/middlewares/movies.m.ts
@@ -0,0 +1,43 @@
+import { hasUndefined } from "../helpers/utils.h";
+import { CheckMoviesRequest, ReserveTimeslotRequest } from "../models/movie.requests"
+import { Request, Response } from "express";
+
+/** 
+ * request validation middleware for {{ list movies route }}
+ * @param {Request} request
+ * @param {Response} response
+ * @param {any} next
+ */
+export function validateCheckMovieRequest(req : Request,res : Response,next : any){
+
+    // fill body object into {{CheckMoviesRequest}}
+    const requestBody = req.body as CheckMoviesRequest
+
+    // check if the requestBody isn't undefiend if that so { Go next() }
+    if(requestBody.movieId && requestBody.timeslotId)
+        return next()
+
+    // otherwise return Bad request response
+    res.send_badRequest("Missing arguments expected")
+}
+
+
+/** 
+ * request validation middleware for {{reserve timeslot route}}
+ * @param {Request} request
+ * @param {Response} response
+ * @param {any} next
+ */
+export function validateReserveTimeslotRequest(req: Request,res: Response,next: any) {
+
+    // fill body object into {{ReserveTimeslotRequest}}
+    const requestBody = req.body as ReserveTimeslotRequest
+
+    // checks if the requestBody isn't undefiend if that so { Go next() }
+    if(requestBody.movieId && requestBody.numberOfreserves && requestBody.timeslotId && requestBody.uid)
+        return next()
+
+    // otherwise return Bad request response
+    return res.send_badRequest("Missing arguments expected")
+
+}
